abaf6a0513ef19a63cfed4d7ae75cc27da09ed69
LogeshKumar <pinglogesh@gmail.com>
2017-02-01 14:59:03 +0530

    added horizontal line program

---
+++ horizontal_line.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class horizontal_line
+{
+    public static void print_horizontal_line(int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            System.out.print("*");   
+        }
+        System.out.println("\n");
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int n;
+	    BufferedReader br=new BufferedReader( new InputStreamReader(System.in));
+	    n=Integer.parseInt(br.readLine());
+		print_horizontal_line(n);
+		
+	}
+}
+
